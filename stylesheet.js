4ed6e12dba6dc0f555496e7e7117d1430eec4440
minhtrangvy <mvy@hmc.edu>
2013-09-20 23:46:29 -0700

    Create stylesheet.js

---
+++ stylesheet.js
@@ -0,0 +1,12 @@
+//how to incorporate external .js file into html cod
+
+<HTML>
+  <HEAD>
+    <SCRIPT LANGUAGE="JavaScript" SRC="myJSfile.js">
+    </SCRIPT>
+  </HEAD>
+    <BODY>
+    <A HREF="javascript:processOrder();">Click here to process your order.</A>
+    ...
+  </BODY>
+</HTML>
